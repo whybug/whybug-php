71939b806176a4c15b3f96803692bb111e2ff5e3
Adrian Philipp <mail@adrian-philipp.com>
2014-07-29 19:44:20 +0200

    Fixes autoload require.

--- auto_pretend.php
+++ auto_pretend.php
@@ -2,6 +2,6 @@
 // Search for a Whybugfile file.
 set_include_path(get_include_path() . DIRECTORY_SEPARATOR . join(DIRECTORY_SEPARATOR, array('/etc/', __DIR__ . '/')));
 
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 Whybug\Tracker::trackErrors(parse_ini_file('Whybugfile.ini'));
 
