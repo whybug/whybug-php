3574f350a9b438c0228e23cca7ea70d5120946fd
Adrian Philipp <mail@adrian-philipp.com>
2014-07-29 20:00:37 +0200

    Moar debugging.

--- src/Whybug/Installer.php
+++ src/Whybug/Installer.php
@@ -8,11 +8,13 @@ class Installer {
     public function postInstall(Event $event)
     {
         var_dump($event);
+        file_put_contents('test1', print_r($event, true));
     }
 
     public function postUpdate(Event $event)
     {
         var_dump($event);
+        file_put_contents('test2', print_r($event, true));
     }
 
     public function install()
