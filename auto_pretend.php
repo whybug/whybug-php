e68bbb17c5b522ba9291fbec5a155d0472c0e984
Adrian Philipp <mail@adrian-philipp.com>
2014-09-20 16:38:17 +0200

    Fixes comment.

--- auto_pretend.php
+++ auto_pretend.php
@@ -1,6 +1,6 @@
 <?php
-// Include the composer autoloader, Whybug is expected in ~/.composer/vendor/whybug/whybug-php.
-require_once __DIR__ . '/../../autoload.php';
+// Include the composer autoloader.
+require_once __DIR__ . '/vendor/autoload.php';
 
 // Set paths for a Whybugfile file.
 set_include_path(get_include_path() . DIRECTORY_SEPARATOR . join(DIRECTORY_SEPARATOR, array('/etc/', __DIR__ . '/')));
