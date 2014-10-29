8064eb5fc28b7052c7fca3d4e37bcee8deb334e8
Adrian Philipp <mail@adrian-philipp.com>
2014-10-29 20:55:06 +0100

    Adds box.json and phar file.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit930903c0311f88049b94885c9b5b5d3f::getLoader();
+return ComposerAutoloaderInitf783490e9142fa5f24c1d160d7288b7a::getLoader();
