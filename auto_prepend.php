ad0ea077ac197c2419808475f928fa621936936c
Adrian Philipp <mail@adrian-philipp.com>
2014-10-29 20:53:27 +0100

    Fixes typo.

--- auto_prepend.php
+++ auto_prepend.php
@@ -1,9 +1,9 @@
 <?php
 // Include the composer autoloader.
-require_once __DIR__ . '/vendor/autoload.php';
+require_once './vendor/autoload.php';
 
 // Set paths for a Whybugfile file.
 set_include_path(get_include_path() . DIRECTORY_SEPARATOR . join(DIRECTORY_SEPARATOR, array('/etc/', __DIR__ . '/')));
 
-Whybug\Tracker::trackErrors(parse_ini_file('Whybugfile.ini'));
+\Whybug\Tracker::trackErrors(parse_ini_file('Whybugfile.ini'));
 
